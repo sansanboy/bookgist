1b6c532de8f18c2e41b79bc1b1e84e814068d195
jim.mac <jim.yan@icloud.com>
2020-04-05 15:33:42 +0700

    deploy

---
+++ precache-manifest.bb5e6a969940eb4703aca6d9c624abe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d9a04024d5384d7c3b",
+    "url": "/bookgist/css/chunk-vendors.23a6cd0c.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/bookgist/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "5680fedcfc4616b4813b16bd882a34cf",
+    "url": "/bookgist/index.html"
+  },
+  {
+    "revision": "a2f725b4e47475995316",
+    "url": "/bookgist/js/app.1cdc7674.js"
+  },
+  {
+    "revision": "f1d9a04024d5384d7c3b",
+    "url": "/bookgist/js/chunk-vendors.74f2e55f.js"
+  },
+  {
+    "revision": "4eefe7b5f05d0362db99de7718f6624f",
+    "url": "/bookgist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookgist/robots.txt"
+  }
+]);
