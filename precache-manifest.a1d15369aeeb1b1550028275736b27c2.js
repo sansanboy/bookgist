42e48be027f272f6c7a89ae736b75023487ea2f2
jim.mac <jim.yan@icloud.com>
2020-04-05 15:21:27 +0700

    deploy

---
+++ precache-manifest.a1d15369aeeb1b1550028275736b27c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d9a04024d5384d7c3b",
+    "url": "/css/chunk-vendors.23a6cd0c.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "84a4ccec60c94a1cdbb1ccf9af2e3acd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "375a194208f45eea4e3c",
+    "url": "/js/app.812353b3.js"
+  },
+  {
+    "revision": "f1d9a04024d5384d7c3b",
+    "url": "/js/chunk-vendors.74f2e55f.js"
+  },
+  {
+    "revision": "4eefe7b5f05d0362db99de7718f6624f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
